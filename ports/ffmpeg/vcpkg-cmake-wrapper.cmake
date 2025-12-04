0ec3865f3bf66fdc971363f713e99862acdb222a
Sergei Lebedev <limentas@gmail.com>
2025-12-04 16:00:27 +0100

    Prefer vcpkg ffmpeg installation over Qt installation to fix build on iOS after update to Qt 6.10

--- ports/ffmpeg/vcpkg-cmake-wrapper.cmake
+++ ports/ffmpeg/vcpkg-cmake-wrapper.cmake
@@ -1,5 +1,6 @@
 set(FFMPEG_PREV_MODULE_PATH ${CMAKE_MODULE_PATH})
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+# We prefer vcpkg installed FFmpeg version over other possible installations
+list(PREPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 cmake_policy(SET CMP0012 NEW)
 
